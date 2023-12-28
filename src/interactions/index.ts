801e6f0a56bdc4758e3a8cde4a972d0805eb5e17
BOTREL Kilian <botrel.kilian1999@gmail.com>
2023-12-28 14:09:01 +0100

    chore(interactions): 🛠️ add skillCalculator to handled interactions

--- src/interactions/index.ts
+++ src/interactions/index.ts
@@ -1,3 +1,5 @@
 import type { InteractionHandler } from 'discordbox'
 
-export const interactions: InteractionHandler[] = []
+import { skillCalculator } from './skillCalculator/interaction.js'
+
+export const interactions: InteractionHandler[] = [skillCalculator]
